f02886dd6b6737f640021d57a494f4636eca28c5
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-01-27 17:18:01 -0300

    Add cel and fah commands

---
+++ src/commands/utility/cel.ts
@@ -0,0 +1,35 @@
+import { Command } from '@sapphire/framework'
+import { type ChatInputCommandInteraction, type InteractionResponse } from 'discord.js'
+
+export class CelCommand extends Command {
+  public constructor (context: Command.LoaderContext, options: Command.Options) {
+    super(context, {
+      ...options,
+      name: 'cel',
+      description: 'Convert temperature from degrees Celsius to degrees Fahrenheit'
+    })
+  }
+
+  public override registerApplicationCommands (registry: Command.Registry): void {
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .addNumberOption((option) =>
+          option
+            .setName('celsius')
+            .setDescription('Temperature in degrees Celsius to convert')
+            .setRequired(true)
+            .setMaxValue(50000)
+            .setMinValue(-50000)
+        ), {
+      idHints: ['1200897088494501978']
+    })
+  }
+
+  public async chatInputRun (interaction: ChatInputCommandInteraction): Promise<InteractionResponse<boolean>> {
+    const celsius = interaction.options.getNumber('celsius', true)
+    const fahrenheit = Math.round(celsius * (9 / 5) + 32)
+    return await interaction.reply(`${celsius}°C is ${fahrenheit}°F`)
+  }
+}
---
+++ src/commands/utility/fah.ts
@@ -0,0 +1,35 @@
+import { Command } from '@sapphire/framework'
+import { type ChatInputCommandInteraction, type InteractionResponse } from 'discord.js'
+
+export class FahCommand extends Command {
+  public constructor (context: Command.LoaderContext, options: Command.Options) {
+    super(context, {
+      ...options,
+      name: 'fah',
+      description: 'Convert temperature from degrees Fahrenheit to degrees Celsius'
+    })
+  }
+
+  public override registerApplicationCommands (registry: Command.Registry): void {
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .addNumberOption((option) =>
+          option
+            .setName('fahrenheit')
+            .setDescription('Temperature in degrees Fahrenheit to convert')
+            .setRequired(true)
+            .setMaxValue(50000)
+            .setMinValue(-50000)
+        ), {
+      idHints: ['1200897090662977598']
+    })
+  }
+
+  public async chatInputRun (interaction: ChatInputCommandInteraction): Promise<InteractionResponse<boolean>> {
+    const fahrenheit = interaction.options.getNumber('fahrenheit', true)
+    const celsius = Math.round((fahrenheit - 32) * (5 / 9))
+    return await interaction.reply(`${fahrenheit}°F is ${celsius}°C`)
+  }
+}
