7f8fce34271671b0109f05eac72d03aa3d269267
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-01-28 14:45:26 -0300

    Add modules command

---
+++ src/commands/guildAdmin/modules.ts
@@ -0,0 +1,92 @@
+import { Command, container } from '@sapphire/framework'
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, PermissionFlagsBits, type ChatInputCommandInteraction } from 'discord.js'
+import Alerts from '../../lib/alerts/alerts'
+import moduleHandler from '../../lib/database/moduleHandler'
+
+export class ModulesCommand extends Command {
+  public constructor (context: Command.LoaderContext, options: Command.Options) {
+    super(context, {
+      ...options,
+      name: 'modules',
+      description: '[SERVER ADMIN ONLY] Enable/disable server modules.',
+      requiredUserPermissions: [PermissionFlagsBits.Administrator]
+    })
+  }
+
+  public override registerApplicationCommands (registry: Command.Registry): void {
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description),
+    {
+      idHints: ['1201207600155480246']
+    })
+  }
+
+  public async chatInputRun (interaction: ChatInputCommandInteraction) {
+    // Fetch module from databases
+    if (interaction.guild === null) return await Alerts.ERROR(interaction, 'This command cannot be run in a DM.', true)
+    const module = await getModuleEmbed(1, interaction.guild.id)
+    if (module instanceof Error) {
+      return await Alerts.ERROR(interaction, 'Error creating embed.', true)
+    }
+    const { embed, components } = module as EmbedObject
+    return await interaction.reply({ embeds: [embed], components, ephemeral: true })
+  }
+}
+
+export interface EmbedObject {
+  embed: EmbedBuilder
+  components: Array<ActionRowBuilder<any>>
+}
+
+export const getModuleEmbed = async (moduleIndex: number, guildId: string) => {
+  try {
+    const module = await container.db.module.findUnique({
+      where: {
+        id: moduleIndex
+      }
+    })
+    if (module === null) throw new Error('Module not found.')
+
+    // First check if module is stored in the database for this guild, if not try to store it
+    const moduleExists = await moduleHandler.checkIfModuleExists(guildId, moduleIndex)
+    if (!moduleExists) throw new Error('Module not found.')
+
+    const pageCount = await container.db.module.count()
+    const queryModule = await container.db.guildModules.findUnique({ where: { guildId_moduleId: { guildId, moduleId: moduleIndex } } })
+    const isModuleEnabled = queryModule?.enabled
+
+    const { name, description } = module
+
+    const embed = new EmbedBuilder()
+      .setColor('Blurple')
+      .setAuthor({ name: 'Server Modules' })
+      .setTitle(name)
+      .setDescription(description)
+      .setFooter({ text: 'Module ' + moduleIndex + '/' + pageCount })
+
+    const prevButton = new ButtonBuilder().setCustomId('prevModule').setLabel('<<').setStyle(ButtonStyle.Secondary).setDisabled(moduleIndex === 1)
+    const enableButton = new ButtonBuilder().setCustomId('enableModule').setLabel('Enable').setStyle(ButtonStyle.Success)
+    const disableButton = new ButtonBuilder().setCustomId('disableModule').setLabel('Disable').setStyle(ButtonStyle.Danger)
+    const nextButton = new ButtonBuilder().setCustomId('nextModule').setLabel('>>').setStyle(ButtonStyle.Secondary).setDisabled(moduleIndex === pageCount)
+
+    // Create action rows
+    const enableRow = new ActionRowBuilder().addComponents(prevButton, enableButton, nextButton)
+    const disableRow = new ActionRowBuilder().addComponents(prevButton, disableButton, nextButton)
+    // Shows if module === 1, which is just an introduction.
+    const neutralRow = new ActionRowBuilder().addComponents(prevButton, nextButton)
+
+    if (moduleIndex === 1) {
+      return { embed, components: [neutralRow] }
+    }
+
+    if (isModuleEnabled === 1) {
+      return { embed, components: [disableRow] }
+    } else {
+      return { embed, components: [enableRow] }
+    }
+  } catch (error) {
+    return error
+  }
+}
---
+++ src/interaction-handlers/guildModulesButtonsHandler.ts
@@ -0,0 +1,73 @@
+import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework'
+import type { ButtonInteraction } from 'discord.js'
+import StringAlerts from '../lib/alerts/stringAlerts'
+import { getModuleEmbed } from '../commands/guildAdmin/modules'
+import guildHandler from '../lib/database/guildHandler'
+import moduleHandler from '../lib/database/moduleHandler'
+
+export class GuildModulesButtonsHandler extends InteractionHandler {
+  public constructor (context: InteractionHandler.LoaderContext, options: InteractionHandler.Options) {
+    super(context, {
+      ...options,
+      interactionHandlerType: InteractionHandlerTypes.Button
+    })
+  }
+
+  public override parse (interaction: ButtonInteraction) {
+    if (interaction.customId !== 'nextModule' && interaction.customId !== 'prevModule' && interaction.customId !== 'enableModule' && interaction.customId !== 'disableModule') return this.none()
+    return this.some()
+  }
+
+  public async run (interaction: ButtonInteraction) {
+    if (interaction.guild === null) return
+    // Pagination
+    if (interaction.customId === 'prevModule' || interaction.customId === 'nextModule') {
+      try {
+        const currentPage = getCurrentIndex(interaction)
+        const page = (interaction.customId === 'prevModule') ? currentPage - 1 : currentPage + 1
+        const { embed, components }: any = await getModuleEmbed(page, interaction.guild.id)
+        return await interaction.update({ content: '', embeds: [embed], components })
+      } catch (error) {
+        console.error(error)
+        return await interaction.update({
+          content: StringAlerts.ERROR('An error has occured while browsing modules. Please try running the /modules command again.')
+        })
+      }
+    }
+
+    if (interaction.customId === 'enableModule' || interaction.customId === 'disableModule') {
+      try {
+        const guildId = interaction.guild.id
+        const currentModule = getCurrentIndex(interaction)
+        // Check if guild is stored in the database first
+        const checkGuild = await guildHandler.updateGuildStatus(guildId)
+        if (!checkGuild) throw new Error('Error checking or storing guild in database')
+
+        if (interaction.customId === 'enableModule') {
+          const enableModule = await moduleHandler.enableModule(guildId, currentModule)
+          if (!enableModule) throw new Error('Error enabling module.')
+        } else {
+          const disableModule = await moduleHandler.disableModule(guildId, currentModule)
+          if (!disableModule) throw new Error('Error disabling module.')
+        }
+
+        const { embed, components }: any = await getModuleEmbed(currentModule, guildId)
+        return await interaction.update({ content: '', embeds: [embed], components })
+      } catch (error) {
+        console.error(error)
+        await interaction.update({
+          content: StringAlerts.ERROR('An error has occured while toggling module. Please try running the /modules command again.')
+        })
+      }
+    }
+  }
+}
+
+const getCurrentIndex = (interaction: ButtonInteraction) => {
+  if (interaction.message.embeds[0].footer === null) throw new Error('Invalid embed.')
+  const pageText = interaction.message.embeds[0].footer.text
+  const slashPos = pageText.indexOf('/')
+  // Module ends at index 7
+  const currentPage = +pageText.substring('Module'.length + 1, slashPos)
+  return currentPage
+}
