d57eaf1e8dc3ed21be679e5052331a73b1b016ee
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-02-03 11:44:37 -0300

    Add OW assets and owhero command

---
+++ src/commands/fun/overwatch/owhero.ts
@@ -0,0 +1,56 @@
+import { Command } from '@sapphire/framework'
+import { AttachmentBuilder, EmbedBuilder, type ChatInputCommandInteraction } from 'discord.js'
+import OverwatchHeroes from '../../../constants/fun/overwatch/heroes'
+import { shuffleArray } from '../../../lib/random/shuffleUtils'
+
+export class OwHeroCommand extends Command {
+  public constructor (context: Command.LoaderContext, options: Command.Options) {
+    super(context, {
+      ...options,
+      name: 'owhero',
+      description: 'Get a random Overwatch hero. Can specify role.'
+    })
+  }
+
+  public override registerApplicationCommands (registry: Command.Registry): void {
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .addStringOption((option) =>
+          option
+            .setName('role')
+            .setDescription('(OPTIONAL) Specify the role you want to get.')
+            .setRequired(false)
+            .addChoices({ name: 'DPS', value: 'dps' }, { name: 'Support', value: 'support' }, { name: 'Tank', value: 'tank' })), {
+      idHints: ['1203344077643317279']
+    })
+  }
+
+  public async chatInputRun (interaction: ChatInputCommandInteraction) {
+    const chosenRole = interaction.options.getString('role') ?? 'notSpecified'
+
+    let chosenHero: string[]
+    if (chosenRole === 'notSpecified') {
+      const allHeroes = [...OverwatchHeroes.DPS_HEROES, ...OverwatchHeroes.SUPPORT_HEROES, ...OverwatchHeroes.TANK_HEROES]
+      chosenHero = shuffleArray(allHeroes)[0]
+    } else if (chosenRole === 'dps') {
+      chosenHero = shuffleArray(OverwatchHeroes.DPS_HEROES)[0]
+    } else if (chosenRole === 'support') {
+      chosenHero = shuffleArray(OverwatchHeroes.SUPPORT_HEROES)[0]
+    } else {
+      chosenHero = shuffleArray(OverwatchHeroes.TANK_HEROES)[0]
+    }
+
+    const [heroName, heroUrl] = chosenHero
+
+    const heroImage = new AttachmentBuilder('../src/assets/overwatchHeroes/' + heroUrl)
+
+    const heroEmbed = new EmbedBuilder()
+      .setColor('Blurple')
+      .setTitle(heroName)
+      .setImage('attachment://' + heroUrl)
+
+    return await interaction.reply({ embeds: [heroEmbed], files: [heroImage] })
+  }
+}
---
+++ src/constants/fun/overwatch/heroes.ts
@@ -0,0 +1,9 @@
+const DPS_HEROES = [['Ashe', 'ashe.png'], ['Bastion', 'bastion.png'], ['Cassidy', 'cassidy.png'], ['Echo', 'echo.png'], ['Genji', 'genji.png'], ['Hanzo', 'hanzo.png'], ['Junkrat', 'junkrat.png'], ['Mei', 'mei.png'], ['Pharah', 'pharah.png'], ['Reaper', 'reaper.png'], ['Sojourn', 'sojourn.png'], ['Soldier: 76', 'soldier76.png'], ['Sombra', 'sombra.png'], ['Symmetra', 'symmetra.png'], ['Torbjörn', 'torbjorn.png'], ['Tracer', 'tracer.png'], ['Widowmaker', 'widowmaker.png']]
+const SUPPORT_HEROES = [['Ana', 'ana.png'], ['Baptiste', 'baptiste.png'], ['Brigitte', 'brigitte.png'], ['Illari', 'illari.png'], ['Kiriko', 'kiriko.png'], ['Lifeweaver', 'lifeweaver.png'], ['Lúcio', 'lucio.png'], ['Mercy', 'mercy.png'], ['Moira', 'moira.png'], ['Zenyatta', 'zenyatta.png']]
+const TANK_HEROES = [['Mauga', 'mauga.png'], ['D.Va', 'dva.png'], ['Doomfist', 'doomfist.png'], ['Junker Queen', 'junkerqueen.png'], ['Orisa', 'orisa.png'], ['Ramattra', 'ramattra.png'], ['Reinhardt', 'reinhardt.png'], ['Roadhog', 'roadhog.png'], ['Sigma', 'sigma.png'], ['Winston', 'winston.png'], ['Wrecking Ball', 'wreckingball.png'], ['Zarya', 'zarya.png']]
+
+const OverwatchHeroes = {
+  DPS_HEROES, SUPPORT_HEROES, TANK_HEROES
+}
+
+export default OverwatchHeroes
