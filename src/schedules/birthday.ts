cdd27ff07e89a03615791d47ad2282655a7a55cb
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-02-02 23:34:40 -0300

    Finish birthday command

--- src/schedules/birthday.ts
+++ src/schedules/birthday.ts
@@ -1,15 +1,15 @@
 import { container } from '@sapphire/framework'
 
-import { addHours, getDate, getDay, getMonth } from 'date-fns'
+import { addHours, getDate, getMonth } from 'date-fns'
 import { scheduleJob } from 'node-schedule'
 import settingsHandler from '../lib/database/settingsHandler'
 import { Settings } from '../constants/bot-config/settings'
+import { checkChannelPermissions } from '../lib/permissions/checkPermissions'
 
 const birthdaySchedule = () => {
-  scheduleJob('* * * * *', async () => {
+  scheduleJob('30 23 * * *', async () => {
     // Fetch all users with all their user preferences
     // If the user has their birthday set to current day, and preference is enabled, send birthday message to channels
-
     const elegibleUsers: ElegibleUsers[] = await container.db.$queryRaw`
     SELECT u.id as userId, g.id as guildId, u.birthday
     FROM User u
@@ -43,17 +43,43 @@ const birthdaySchedule = () => {
       const currentMonth = getMonth(today)
 
       if (currentDay === day && currentMonth === month) {
-        await sendBirthdayMessage(user.guildId, user.userId)
+        const sent = await sendBirthdayMessage(user.guildId, user.userId)
+        if (sent === false) console.log('Error sending birthday message to ' + user.userId + ' in guild ' + user.guildId)
       }
     }
   })
 }
 
 const sendBirthdayMessage = async (guildId: string, userId: string) => {
-  // Get main channel from server preferences
-  const mainChannel = settingsHandler.fetchSetting(guildId, Settings.mainchannel)
-  if (mainChannel === null) {
-    container.client.channels.cache.get(mainChannel)
+  console.log('Sending birthday message for ' + userId + ' in guild ' + guildId)
+  try {
+    if (container.client.user == null) return false
+    // Get main channel from server preferences and send text message to it
+    const mainChannel = await settingsHandler.fetchSetting(guildId, Settings.mainchannel)
+    if (mainChannel != null) {
+      const channel = container.client.channels.cache.get(mainChannel)
+      if (channel != null && channel.isTextBased()) {
+        if (await checkChannelPermissions(guildId, container.client.user.id, channel.id)) {
+          return await channel.send(`Today is **<@${userId}>**'s birthday! Happy birthday! :tada:`)
+        }
+      }
+    }
+
+    // Send message to first available text channel if there isn't a main channel or bot doesn't have permissions
+    const channels = (await (container.client.guilds.fetch(guildId))).channels.cache.filter((ch) => ch.isTextBased() && !ch.isVoiceBased() && !ch.isThread() && !ch.isThreadOnly())
+    for (const ch of channels) {
+      const channel = ch[1]
+      if (channel == null) continue
+      if (channel.isTextBased()) {
+        if (await checkChannelPermissions(guildId, container.client.user.id, channel.id)) {
+          return await channel.send(`Today is <@${userId}>'s birthday! **Happy birthday!** :tada:`)
+        }
+      }
+    }
+    return false
+  } catch (error) {
+    console.error(error)
+    return false
   }
 }
 
