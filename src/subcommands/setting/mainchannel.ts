407a443b333012bce6d47974fcb4e5984501e1b3
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-02-02 23:46:36 -0300

    Add permission check to mainchannel subcommand

--- src/subcommands/setting/mainchannel.ts
+++ src/subcommands/setting/mainchannel.ts
@@ -2,6 +2,7 @@ import { type ChatInputCommandInteraction } from 'discord.js'
 import Alerts from '../../lib/alerts/alerts'
 import settingsHandler from '../../lib/database/settingsHandler'
 import { Settings } from '../../constants/bot-config/settings'
+import { checkChannelPermissions } from '../../lib/permissions/checkPermissions'
 
 export const updateMainChannel = async (interaction: ChatInputCommandInteraction) => {
   if (interaction.guild === null) throw new Error('This command cannot be executed in a DM.')
@@ -10,6 +11,9 @@ export const updateMainChannel = async (interaction: ChatInputCommandInteraction
 
   try {
     if (value == null) throw new Error('You need to specify a valid channel.')
+
+    const checkPermissions = await checkChannelPermissions(guildId, interaction.client.user.id, value)
+    if (!checkPermissions) return await Alerts.ERROR(interaction, 'I don\'t have permission to see and/or send messages in that channel.\nPlease change the permissions or choose another channel.', true)
 
     const update = await settingsHandler.updateSetting(guildId, Settings.mainchannel, value)
     if (!update) throw new Error('An error occurred while updating the main channel.')
