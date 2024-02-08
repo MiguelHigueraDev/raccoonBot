0584a50470e3efb771dbd06c810b1f551730c7f7
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-02-08 20:55:54 -0300

    feat: add movie command + update env.example

---
+++ src/commands/fun/tmdb/movie.ts
@@ -0,0 +1,118 @@
+import { Command } from '@sapphire/framework'
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, type ChatInputCommandInteraction } from 'discord.js'
+import { config } from 'dotenv'
+import { type CastResult, type Cast, type Movie, type MovieResult, type MovieResults, type KeywordsResult, type Keyword } from '../../../lib/interface/tmdb'
+import Alerts from '../../../lib/alerts/alerts'
+import { ShowLoadingScreen } from '../../../lib/api/loadingMessage'
+config()
+const TMDB_KEY = process.env.TMDB_KEY
+
+export class MovieCommand extends Command {
+  public constructor (context: Command.LoaderContext, options: Command.Options) {
+    super(context, {
+      ...options,
+      name: 'movie',
+      description: 'Get information about a movie from The Movie Database.',
+      cooldownDelay: 3000
+    })
+  }
+
+  public override registerApplicationCommands (registry: Command.Registry): void {
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .addStringOption((option) =>
+          option
+            .setName('movie')
+            .setDescription('The name of the movie.')
+            .setRequired(true)
+        ), {
+      idHints: ['1205276006420709427']
+    })
+  }
+
+  public async chatInputRun (interaction: ChatInputCommandInteraction) {
+    const movie = interaction.options.getString('movie', true)
+    const loadingMessage = await ShowLoadingScreen(interaction)
+    try {
+      const partialMovieQuery = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&query=${movie}&include_adult=false`)
+      const partialMovieQueryData: MovieResults = await partialMovieQuery.json()
+
+      if (partialMovieQueryData.results != null && partialMovieQueryData.results.length > 0) {
+        const partialMovieJson: MovieResult = partialMovieQueryData.results[0]
+        const id = partialMovieJson.id
+        // Now fetch more data about the movie
+        const fullMovieQuery = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${TMDB_KEY}`)
+        const fullMovieData: Movie = await fullMovieQuery.json()
+        const {
+          original_title: originalTitle, title: movieTitle,
+          overview: movieSynopsis, release_date: releaseDate, genres,
+          poster_path: posterUrl
+        } = fullMovieData
+
+        // Format movie data
+        const releaseYear = releaseDate.substring(0, 4)
+        const genreString = genres.map((genre) => genre.name).join(', ')
+        let finalTitle = ''
+        if (originalTitle === movieTitle) {
+          finalTitle = movieTitle
+        } else {
+          finalTitle = `${movieTitle} (${originalTitle})`
+        }
+
+        // Fetch cast
+        const castQuery = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${TMDB_KEY}`)
+        const castData: CastResult = await castQuery.json()
+        const selectedCast: Cast[] = castData.cast.slice(0, 7)
+        const castString = selectedCast.map((actor) => actor.name).join(', ')
+
+        // Fetch keywords
+        const keywordsQuery = await fetch(`https://api.themoviedb.org/3/movie/${id}/keywords?api_key=${TMDB_KEY}`)
+        const keywordsData: KeywordsResult = await keywordsQuery.json()
+        const selectedKeywords: Keyword[] = keywordsData.keywords.slice(0, 10)
+        const keywordsString = selectedKeywords.map((keyword) => keyword.name).join(', ')
+
+        // Make embed and button
+        const embed = this.makeEmbed(finalTitle, posterUrl, releaseYear, genreString, movieSynopsis, castString, keywordsString)
+        const buttonRow = this.makeWatchButton(id)
+
+        // Check if message was deleted to prevent crash before editing loading
+        const msgStillExists = await interaction.channel?.messages.fetch(loadingMessage.id).catch(() => null)
+        if (msgStillExists == null) return
+        await loadingMessage.edit({ embeds: [embed], components: [buttonRow] })
+      } else {
+        return await Alerts.WARN(interaction, 'Movie not found.', true)
+      }
+    } catch (error) {
+      console.error(error)
+      return await Alerts.ERROR(interaction, 'Error fetching data about the movie.', true)
+    }
+  }
+
+  private makeEmbed (title: string, posterUrl: string, releaseYear: string, genreString: string, movieSynopsis: string, castString: string, keywordsString: string) {
+    const movieEmbed = new EmbedBuilder()
+      .setColor('Blurple')
+      .setTitle(title)
+      .setDescription(movieSynopsis)
+      .setFooter({ text: 'Movie information provided by The Movie Database' })
+      .setThumbnail(`https://image.tmdb.org/t/p/original${posterUrl}`)
+      .addFields(
+        { name: 'Main Cast', value: castString },
+        { name: 'Genres', value: genreString },
+        { name: 'Keywords', value: keywordsString },
+        { name: 'Release Year', value: releaseYear }
+      )
+    return movieEmbed
+  }
+
+  private makeWatchButton (movieId: number) {
+    const button = new ButtonBuilder()
+      .setLabel('Where to watch')
+      .setStyle(ButtonStyle.Link)
+      .setURL(`https://www.themoviedb.org/movie/${movieId}/watch`)
+
+    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button)
+    return row
+  }
+}
---
+++ src/lib/interface/tmdb.ts
@@ -0,0 +1,81 @@
+export interface MovieResults {
+  page: number
+  results: MovieResult[]
+  total_pages: number
+  total_results: number
+}
+
+export interface MovieResult {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: any[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+export interface Movie {
+  adult: boolean
+  backdrop_path: string
+  belongs_to_collection: any
+  budget: number
+  genres: any[]
+  homepage: string
+  id: number
+  imdb_id: string
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  production_companies: any[]
+  production_countries: any[]
+  release_date: string
+  revenue: number
+  runtime: number
+  spoken_languages: any[]
+  status: string
+  tagline: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+export interface CastResult {
+  cast: Cast[]
+  id: number
+}
+
+export interface Cast {
+  adult: false
+  gender: number
+  id: number
+  known_for_department: string
+  name: string
+  original_name: string
+  popularity: number
+  profile_path: string
+  cast_id: number
+  character: string
+  credit_id: string
+  order: number
+}
+
+export interface KeywordsResult {
+  id: number
+  keywords: Keyword[]
+}
+
+export interface Keyword {
+  id: number
+  name: string
+}
