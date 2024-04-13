51465cdfac97d43536708cf21873e3551179a8b2
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-13 18:37:49 -0400

    refactor(pollButtonsHandler): refactor to improve clarity

--- src/interaction-handlers/pollButtonsHandler.ts
+++ src/interaction-handlers/pollButtonsHandler.ts
@@ -18,8 +18,15 @@ export class PollButtonsHandler extends InteractionHandler {
     return this.some()
   }
 
+  /**
+   * This function is called when a poll buton is clicked by a user
+   * It handles votes and updates the poll embed with the new vote count
+   * It only updates the poll embed when there is a valid vote
+   * @param interaction The button interaction
+   * @returns void
+   */
   public async run (interaction: ButtonInteraction) {
-    // Get ID from embed footer
+    // Get the poll ID from the embed footer, making sure that there is a number present. If not, return early.
     const embed = interaction.message.embeds[0]
     if (embed.footer == null) return
     const footer = embed.footer.text
@@ -34,22 +41,22 @@ export class PollButtonsHandler extends InteractionHandler {
 
     // Get Option number from button custom ID
     const optionNumber = Number(interaction.customId.substring('pollOption'.length))
-    // Replace the emojis at the start with an empty string
+    // Replace the emojis at the start with an empty string to get the option text
     const optionText = embed.fields[optionNumber - 1].name.replace(/:(one|two|three|four|five|six|seven|eight): /g, '')
     // Finally get the optionId
     const optionId: number = await pollHandler.getOptionId(pollId, optionText)
-
     if (optionId == null) return await interaction.reply({ content: StringAlerts.ERROR('Error fetching poll data.'), ephemeral: true })
 
+    // Check if user has already voted
     const checkAlreadyVoted = await pollHandler.checkIfUserVoted(pollId, interaction.user.id)
     if (checkAlreadyVoted === true) {
       return await interaction.reply({ content: StringAlerts.WARN('You have already voted in this poll. You can\'t vote again.'), ephemeral: true })
-    } else {
-      // Add vote
-      await pollHandler.addVote(pollId, interaction.user.id, optionId)
     }
 
-    // Get votes and extra info
+    // All checks have passed, add the user's vote to the poll.
+    await pollHandler.addVote(pollId, interaction.user.id, optionId)
+
+    // Get votes and extra info to update the poll embed
     const votes: VoteObject[] = await pollHandler.getVotes(pollId)
     if (votes == null) return await interaction.reply({ content: StringAlerts.ERROR('Error fetching poll data.'), ephemeral: true })
     const totalVotes: number = votes.reduce((total, vote) => Number(total) + Number(vote.voteCount), 0)
@@ -58,37 +65,49 @@ export class PollButtonsHandler extends InteractionHandler {
 
     // Get question from votes array
     const question = votes[0].question
-    // Update embed and vote count
-    const pollEmbed = await getPollEmbed(pollId, question, totalVotes, votes, creatorId, expirationDate)
+    // Finally update the embed and vote count
+    const pollEmbed = await this.getPollEmbed(pollId, question, totalVotes, votes, creatorId, expirationDate)
     await interaction.update({ embeds: [pollEmbed] })
   }
-}
 
-const getPollEmbed = async (pollId: number, question: string, total: number, votes: VoteObject[], creatorId: string, expirationDate: Date) => {
-  // Get creator data
-  const creator = await container.client.users.fetch(creatorId)
-  const expirationDateUnix = Math.floor(expirationDate.getTime() / 1000)
-  const pollEmbed = new EmbedBuilder()
-    .setColor('Blurple')
-    .setTitle(question)
-    .setFooter({ text: `ID: ${pollId}`, iconURL: creator.displayAvatarURL() })
+  /**
+   * Retrieves the poll embed for a given poll.
+   *
+   * @param {number} pollId - The ID of the poll.
+   * @param {string} question - The question of the poll.
+   * @param {number} total - The total number of votes in the poll.
+   * @param {VoteObject[]} votes - An array of vote objects containing the option text and vote count.
+   * @param {string} creatorId - The ID of the poll creator.
+   * @param {Date} expirationDate - The expiration date of the poll.
+   * @returns {EmbedBuilder} The poll embed.
+   */
+  private async getPollEmbed (pollId: number, question: string, total: number, votes: VoteObject[], creatorId: string, expirationDate: Date) {
+  // Get poll creator data
+    const creator = await container.client.users.fetch(creatorId)
+    const expirationDateUnix = Math.floor(expirationDate.getTime() / 1000)
+    const pollEmbed = new EmbedBuilder()
+      .setColor('Blurple')
+      .setTitle(question)
+      .setFooter({ text: `ID: ${pollId}`, iconURL: creator.displayAvatarURL() })
 
-  // Add options
-  for (let i = 0; i < votes.length; i++) {
-    const emoji = emojiMap[i + 1]
-    const percentage = Math.round((Number(votes[i].voteCount) / Number(total)) * 100)
-    const percentageBar = getPercentageBar(percentage)
-    const voteString = Number(votes[i].voteCount) === 1 ? 'vote' : 'votes'
-    pollEmbed.addFields({
-      name: `${emoji} ${votes[i].optionText}`,
-      value: `${percentageBar} ${percentage}% (${Number(votes[i].voteCount)} ${voteString})`
-    })
+    // Add all the poll's options with their respective votes and percentages
+    for (let i = 0; i < votes.length; i++) {
+      const emoji = emojiMap[i + 1]
+      const percentage = Math.round((Number(votes[i].voteCount) / Number(total)) * 100)
+      const percentageBar = getPercentageBar(percentage)
+      const voteString = Number(votes[i].voteCount) === 1 ? 'vote' : 'votes'
+      pollEmbed.addFields({
+        name: `${emoji} ${votes[i].optionText}`,
+        value: `${percentageBar} ${percentage}% (${Number(votes[i].voteCount)} ${voteString})`
+      })
+    }
+    pollEmbed.addFields({ name: `Total votes: ${Number(total)}`, value: `Expires <t:${expirationDateUnix}:R>` })
+    return pollEmbed
   }
-  pollEmbed.addFields({ name: `Total votes: ${Number(total)}`, value: `Expires <t:${expirationDateUnix}:R>` })
-  return pollEmbed
 }
 
-export interface VoteObject {
+// Represents a vote in the database.
+interface VoteObject {
   question: string
   userId: string
   expirationDate: Date
